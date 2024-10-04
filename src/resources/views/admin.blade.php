d4924e9b24c23aa69e0e9725994f20d35d9fde56
Gerolsteiner <solid.absorption@gmail.com>
2024-10-04 13:41:28 +0900

    正常動作。adminのログアウトボタン実装。

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -14,30 +14,49 @@
     <header class="header">
         <div class="header__inner">
         <a class="header__logo" href="/">
-            Contact Form
+            FashionablyLate
         </a>
+                    <div class="logout__button">
+                <form action="/logout" method="post">
+                    @csrf
+                    <button class="logout__button-submit" type="submit">logout</button>
+                </form>
+            </div>
         </div>
     </header>
 
     <main>
         <div class="thanks__content">
             <div class="thanks__heading">
-                <h2>お問い合わせありがとうございます</h2>
-
-<div class="pagination-wrapper">
-    {{ $contacts->links() }}        <!-- ページネーションリンクを表示 -->
-</div>
-                <ul>
-        @foreach ($contacts as $contact)
-            <li>{{ $contact->first_name }}  {{ $contact->last_name }} - {{ $contact->gender_label }} - {{ $contact->email }} - {{  $contact->category->content }}</li>
-        @endforeach
-                </ul>
-
+                <h2>Admin</h2>
             </div>
-            <form class="thanks__form" action="/" method="get">
-                <div class="thanks__button">
-                    <button type="submit">Home</button>
+
+            <form class="form" action="/xxxx" method="xxx">
+                @csrf
+
+                <div class="admin__group">
+                    <div>
+                        <button>エクスポート</button>
+                    </div>
+                    <div>
+                    {{ $contacts->links() }}    <!-- ページネーションリンクを表示 -->
+                    </div>
                 </div>
+
+                <div class="admin__group">
+                    <div>
+                        <ul>
+                        @foreach ($contacts as $contact)
+                            <li>{{ $contact->first_name }}  {{ $contact->last_name }} -
+                                {{ $contact->gender_label }} -
+                                {{ $contact->email }} -
+                                {{  $contact->category->content }}
+                            </li>
+                        @endforeach
+                        </ul>
+                    </div>
+                </div>
+
             </form>
         </div>
     </main>
